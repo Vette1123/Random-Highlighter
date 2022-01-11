406dc4b2ab4c7300742f357b0b0f08a22438fee5
MohamedGado <boogado@yahoo.com>
2022-01-11 18:12:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+const TagsDiv = document.getElementById("tags");
+const TextArea = document.querySelector(".textarea");
+
+TextArea.focus();
+
+TextArea.addEventListener("keyup", (event) => {
+  createTags(event.target.value);
+  if (event.key === "Enter") {
+    setTimeout(() => {
+      event.target.value = "";
+    }, 10);
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  TagsDiv.innerHTML = "";
+
+  tags.forEach((tagElement) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tagElement;
+    TagsDiv.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  const times = 30;
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highLightTag(randomTag);
+
+    setTimeout(() => {
+      unhighLightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highLightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+function highLightTag(tag) {
+  tag.classList.add("highlight");
+}
+function unhighLightTag(tag) {
+  tag.classList.remove("highlight");
+}
